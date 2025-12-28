996114d8d4a2d168892d527c804a86936529e329
Josue <geenujunior@gmail.com>
2025-12-28 04:20:38 -0300

    feat: linkando usuários aos contatos

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -14,7 +14,7 @@ from contact.forms import ContactForm
 @login_required(login_url='contact:login')
 def update(request, contact_id):
 
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
 
     form_action = reverse('contact:update', args=(contact_id,))
 
@@ -61,7 +61,9 @@ def create(request):
 
         # Só retorna true se o form não tiver NENHUM erro
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id = contact.id)
 
         return render(request, 'contact/create.html', context)
@@ -77,7 +79,7 @@ def create(request):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
 
-    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True, owner=request.user)
     # contact.delete()
     # return redirect('contact:index')
 
