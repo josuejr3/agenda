176ce89947329de72633ed0e237c032e2ba7c911
Josue <geenujunior@gmail.com>
2025-12-28 00:57:14 -0300

    fix: ajuste na criação de usuários

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 from . import models
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
 
@@ -96,4 +97,33 @@ class ContactForm(forms.ModelForm):
         return first_name
 
 class RegisterForm(UserCreationForm):
-    ...
+
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
+
+
